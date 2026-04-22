9ceb790f6fb22ef9637cc2d2f22d2b1874ee2741
Harsh8301 <ugaleharsh8301@gmail.com>
2026-04-22 08:28:08 +0530

    Update login.php

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 include 'db.php';
 
 $email = $_POST['email'];
-password_verify($password, $row['password']);
+$password = $_POST['password'];
 
 $sql = "SELECT * FROM users WHERE email='$email' AND password='$password'";
 $result = $conn->query($sql);
